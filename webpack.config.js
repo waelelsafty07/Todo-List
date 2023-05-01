aff2905ae60288d8a6d53713e06eeeb78fb6ca80
wael elsafty <wael.elsafty@bld.ai>
2023-05-02 01:40:21 +0300

    add html and css structure To-do list app

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'To-Do List',
       template: './src/index.html',
     }),
   ],
