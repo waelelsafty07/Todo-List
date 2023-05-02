d7c5463dd3b48964f4f4b7e4dd83a2f881f783d2
wael elsafty <wael.elsafty@bld.ai>
2023-05-02 12:04:37 +0300

    add style to checkbox and fix child to do list child

--- src/js/createElement.js
+++ src/js/createElement.js
@@ -2,7 +2,6 @@ const createElement = (tag, task = undefined) => {
   const elements = document.createElement(tag);
   if (tag === 'li') {
     elements.className = 'to-do-list-item';
-    if (task.completed) elements.classList.add('completed');
     return elements;
   }
   if (tag === 'input') {
@@ -13,6 +12,7 @@ const createElement = (tag, task = undefined) => {
   }
   if (tag === 'span') {
     elements.innerHTML = task.description;
+    if (task.completed) elements.classList.add('completed');
     return elements;
   }
   return elements;
--- src/js/eventComplete.js
+++ src/js/eventComplete.js
@@ -2,7 +2,7 @@ const eventComplete = () => {
   const listItems = document.querySelectorAll('.to-do-list-item');
   listItems.forEach((item) => {
     item.addEventListener('change', () => {
-      item.classList.toggle('completed');
+      item.querySelector('span').classList.toggle('completed');
     });
   });
 };
