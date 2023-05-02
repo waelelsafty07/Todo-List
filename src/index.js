62e4ae3041b8a990096db0bc2caf8bc9392084d7
wael elsafty <wael.elsafty@bld.ai>
2023-05-02 11:21:50 +0300

    use function iterate tasks to be executable

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-import _ from 'lodash';
 import './style.css';
+import lists from './js/listData.js';
+import iterateTasks from './js/iterateTasks.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  //  Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
-
-document.body.appendChild(component());
+iterateTasks(lists);
